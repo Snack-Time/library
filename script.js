f68595a25f38fe6c52f215a56b7e99e550a3fa60
Snack-Time <wmcnally1226@gmail.com>
2022-11-27 00:58:11 -0500

    added form in html

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-p
+let myLibrary = [];
+
+function book(title, author, pages, read) {
+    title = this.title
+    author = this.author
+    pages = this.pages
+    read = this.read
+};
+
+function addBook() {
+
+};
